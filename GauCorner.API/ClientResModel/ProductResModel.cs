9161462fe0b8c310b5875e202efb236bed315e8e
Đoàn Ngọc Minh Quân <minhquandoanngoc@gmail.com>
2025-06-16 17:34:32 +0700

    [Fix] AttributeImage allow null

--- GauCorner.API/ClientResModel/ProductResModel.cs
+++ GauCorner.API/ClientResModel/ProductResModel.cs
@@ -1,6 +1,6 @@
 public class ProductFormWrapper
 {
     public string Product { get; set; } // JSON string
-    public IFormFile[] AttributeImage { get; set; } // Files theo index
+    public IFormFile[]? AttributeImage { get; set; } // Files theo index
     public IFormFile[] ProductImage { get; set; } // Files theo index
 }
